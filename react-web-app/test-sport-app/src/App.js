0e6f9f0b1c0914efdd72eefa4b92d98c02c09f8a
shubhp <shubh@neocortex.com>
2022-12-08 13:05:49 -0800

    setup and webcam component

---
+++ react-web-app/test-sport-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Webcam from "react-webcam";
+
+function App() {
+  return (
+    <div className="App">
+      <Webcam />
+      
+    </div>
+  );
+}
+
+export default App;
